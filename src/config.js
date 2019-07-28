5d965e1c3d6eee3e3e986cec777b62afd7d79378
Fred Policarpo <frederico.policarpo@gmail.com>
2019-07-28 15:55:21 -0300

    Add integration with the AWS Backend using the AWS Amplify library

---
+++ src/config.js
@@ -0,0 +1,17 @@
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "policarpo-notes-app-uploads"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://d61r536v62.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_uYfrNEk5L",
+      APP_CLIENT_ID: "7394qtpseldu9qa092mks37ttr",
+      IDENTITY_POOL_ID: "us-east-2:def0a4ef-3aad-465d-a767-789d30079497"
+    }
+  };
+  
